27597ef7474e75899f1315c0f239aaf13b32c919
Coder2142 <72675065+Coder2142@users.noreply.github.com>
2021-05-20 20:53:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var database;
 var foodS=0;
 var foodObj;
 var milk;
+var lastFed;
+var fedTime;
 
 
 function preload(){
@@ -38,10 +40,26 @@ function draw() {
   background(46,139,87);
 
   foodObj.display();
+  fedTime=database.ref("FeedTime");
+  fedTime.on("value",function(data){
+   lastFed=data.val();
+  });
+
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed :" + lastFed%12 + "PM",350,30);
+    
+  }else if(lastFed==0){
+    text("Last Feed : 12 AM",350,30);
+  }else{
+    text("Last Feed : " + lastFed + "AM",350,30);
+  }
   
   
   drawSprites();
 }
+
 
 //function to read food Stock
 
@@ -49,15 +67,16 @@ function draw() {
 //function to update food stock and last fed time
 function Feed(){
   dog.addImage(happyDog);
+  
+  foodS = foodObj.getFoodStock();
+foodObj.updateFoodStock(foodS-1);
+  database.ref('/').update({
+    foodStock:foodObj.getFoodStock(),
+    FeedTime:hour()
+    
+  })
 
-  foodS= foodObj.getFoodStock(); //added here
-  if(foodS<= 0){
-    foodObj.updateFoodStock(foodS*0);
-  } else {
-    foodObj.updateFoodStock(foodS-1);
   }
-  foodS= foodObj.getFoodStock(); //added here
-}
 
 //function to add food in stock
 function stockFood(){
